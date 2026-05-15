52eb640f9523f987caa6dbe8e6a430a8ab144b64
ClaroLogistica <roger.silva.terceiros@claro.com.br>
2026-05-15 15:26:21 -0300

    Update script_dia.js

--- script_dia.js
+++ script_dia.js
@@ -46,7 +46,7 @@ fetch(new URL("Dados.xlsx", window.location.href))
     console.log("✅ Excel carregado:", dados.length);
     atualizarTudo();
   })
-  .ch(err => console.error(err));
+  .catch(err => console.error(err));
 
 /*************************************************
  * FILTRO CENTRAL
@@ -55,21 +55,22 @@ function aplicarFiltros() {
   let base = [...dados];
 
   if (filtroLocais.length > 0) {
-  base = base.filter(d =>
-    filtroLocais.some(f => d.Local && d.Local.includes(f))
-  );
+    base = base.filter(d =>
+      filtroLocais.some(f => d.Local && d.Local.includes(f))
+    );
+  }
 
   if (filtroTipo && filtroTecnologias.length > 0) {
     base = base.filter(d => filtroTecnologias.includes(d[filtroTipo]));
   }
 
   if (filtroSemanaSelecionada) {
-  base = base.filter(d => {
-    let s = Object.keys(d).find(k => k.toLowerCase().includes("semana"));
-    return s && d[s] && d[s].includes(filtroSemanaSelecionada);
-  });
-}
-    
+    base = base.filter(d => {
+      let s = Object.keys(d).find(k => k.toLowerCase().includes("semana"));
+      return s && d[s] && d[s].includes(filtroSemanaSelecionada);
+    });
+  }
+
   if (filtroGiro.length > 0) {
     base = base.filter(d => filtroGiro.includes(d.Giro));
   }
