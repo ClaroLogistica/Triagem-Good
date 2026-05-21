10bf4f18b22c332a25764e00724acd79936ee8ff
ClaroLogistica <roger.silva.terceiros@claro.com.br>
2026-05-21 15:36:13 -0300

    Update script_dia.js

--- script_dia.js
+++ script_dia.js
@@ -192,8 +192,6 @@ function atualizarGrafico() {
 }
 
   atualizarFaixaSemanas(base);
-}
- 
 /*************************************************
  * SEMANAS
  *************************************************/
